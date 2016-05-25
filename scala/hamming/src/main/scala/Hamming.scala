b8071ff9fbd35ecbee909ff074fe08926023efe1
LEEJeyeon <ljy@LEEJeyeonui-MacBook-Pro.local>
2016-05-25 10:39:23 +0900

    Use zip function. found diffrent way

--- scala/hamming/src/main/scala/Hamming.scala
+++ scala/hamming/src/main/scala/Hamming.scala
@@ -5,16 +5,23 @@ import scala.annotation.tailrec
   */
 object Hamming {
 
+//  def compute(first: String, second: String): Int = {
+//    if (first.size != second.size) throw new IllegalArgumentException
+//    compute(first, second, 0, 0);
+//  }
+//
+//  @tailrec
+//  def compute(first: String, second: String, index: Int, acc: Int): Int = {
+//    if (first.size <= index) return acc
+//    if (!first.charAt(index).equals(second.charAt(index))) compute(first, second, index + 1, acc + 1)
+//    else compute(first, second, index + 1, acc)
+//  }
+
   def compute(first: String, second: String): Int = {
     if (first.size != second.size) throw new IllegalArgumentException
-    compute(first, second, 0, 0);
+    var diff = 0;
+    def addDiff = {diff += 1}
+    first.zip(second).foreach((x)=>{ if (x._1 != x._2) {addDiff}})
+    diff
   }
-
-  @tailrec
-  def compute(first: String, second: String, index: Int, acc: Int): Int = {
-    if (first.size <= index) return acc
-    if (!first.charAt(index).equals(second.charAt(index))) compute(first, second, index + 1, acc + 1)
-    else compute(first, second, index + 1, acc)
-  }
-
 }
